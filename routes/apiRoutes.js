a6502157a2db6295d8335063f4ac1e04f96a2a81
Ashley Anness <ashleyaanness@gmail.com>
2020-11-10 21:17:26 -0500

    updated server.js and routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,52 @@
-var data = require("../db/notesData");
 const fs = require("fs");
-const notesData = require("../db/notesData");
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 
+
+module.exports = function(app) {
     app.get("api/notes", (req, res) => {
         res.json(notesData);
     });
 
-    app.post("api/notes/", (req, res) => {
-        notesData.push(req.body);
-        fs.writeFileSync("./db/db.json", JSON.stringify(notesData), function(err) {
-            if (err) throw (err);        
-        }); 
-    
-        res.json(true);    
-    
+    app.get("/api/notes/:id", (req, res) => {
+        res.json(data[Number(req.params.id)]);
+
     });
 
-    app.delete("api/notes/:note", (req, res) => {
-        let id = req.params.note;
-        notesData - notesData.filter(data => data.id !==id)
-        fs.writeFileSync("./db/db.json", JSON.stringify(notesData));
-        res.json({ok: true});
+    app.post("/api/notes", (req, res) => {
+
+        let newNote = req.body;
+        let uniqueId = (data.length).toString();
+        console.log(uniqueId);
+        newNote.id = uniqueId;
+        data.push(newNote);
+        
+        fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
+            if (err) throw (err);        
+        }); 
+
+        res.json(data);    
+
+    });
+
+    app.delete("/api/notes/:id", (req, res) => {
+
+        let noteId = req.params.id;
+        let newId = 0;
+        console.log(`Deleting note with id ${noteId}`);
+        data = data.filter(currentNote => {
+           return currentNote.id != noteId;
+        });
+        for (currentNote of data) {
+            currentNote.id = newId.toString();
+            newId++;
+        }
+        fs.writeFileSync("./db/db.json", JSON.stringify(data));
+        res.json(data);
     }); 
+}
+    
 
-
-
-module.exports = notesData;
 
 
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,17 @@
 const path = require("path");
-const notesData = require("../db/notesData");
+
+module.exports = function(app) {
+    
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/index.html"));
+    });
+    
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/notes.html"));
+    });
+
+    
 
 
-
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-module.exports = notesData;
+}
 
